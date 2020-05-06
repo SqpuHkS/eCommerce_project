d18634cf4cb9d186575259fea1950e488d56a84f
SqpuHkS <kovaleksey01@gmail.com>
2020-05-06 22:20:01 +0300

    added model for items

--- eCommerce_project/models.py
+++ eCommerce_project/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Item(models.Model):
+    title = models.CharField(max_length=30)
+    description = models.TextField()
+    price = models.DecimalField(decimal_places=2, max_digits=20, default=0)
+    image = models.ImageField(upload_to='items/', null=True, blank=True)
+
