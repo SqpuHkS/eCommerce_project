960c522003bea4b0789ba9c2dfe2dc18277f9cf0
SqpuHkS <kovaleksey01@gmail.com>
2020-05-14 22:59:13 +0300

    edit the urls

--- eCommerce_project/urls.py
+++ eCommerce_project/urls.py
@@ -1,10 +1,9 @@
-from django.urls import path
 from .views import *
 from django.conf.urls import url
 
 urlpatterns = [
-    path('', main_page, name='main-page-url'),
-    path('login/', login_page, name='login-page-url'),
-    path('register/', register_page, name='register-page-url'),
+    url('', main_page, name='main-page-url'),
+    url('login/', login_page, name='login-page-url'),
+    url('register/', register_page, name='register-page-url'),
     url('items/(?P<slug>[\w-]+)/$', ItemDetailSlugView.as_view())
 ]
