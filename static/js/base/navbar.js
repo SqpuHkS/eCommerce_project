ea4db66f579d76338f99f54cc10cc459d448a7d7
SqpuHkS <kovaleksey01@gmail.com>
2020-06-02 23:45:04 +0300

    added a new search bar and remake the home page

--- static/js/base/navbar.js
+++ static/js/base/navbar.js
@@ -1,41 +1,38 @@
-$(function(){
-if($('.nav>ul>li').hasClass('selected')){
+$(function () {
+    if ($('.nav>ul>li').hasClass('selected')) {
+        $('.selected').addClass('active');
+        var currentleft = $('.selected').position().left + "px";
+        var currentwidth = $('.selected').css('width');
+        $('.lamp').css({"left": currentleft, "width": currentwidth});
 
-$('.selected').addClass('active');
-var currentleft=$('.selected').position().left+"px";
-var currentwidth=$('.selected').css('width');
-$('.lamp').css({"left":currentleft,"width":currentwidth});
+    } else {
 
-}
-else{
+        $('.nav>ul>li').first().addClass('active');
+        var currentleft = $('.active').position().left + "px";
+        var currentwidth = $('.active').css('width');
+        $('.lamp').css({"left": currentleft, "width": currentwidth});
+    }
 
-  $('.nav>ul>li').first().addClass('active');
-  var currentleft=$('.active').position().left+"px";
-var currentwidth=$('.active').css('width');
-$('.lamp').css({"left":currentleft,"width":currentwidth});
-}
 
-$('.nav>ul>li').hover(function(){
-  $('.nav ul li').removeClass('active');
-  $(this).addClass('active');
-var currentleft=$('.active').position().left+"px";
-var currentwidth=$('.active').css('width');
-$('.lamp').css({"left":currentleft,"width":currentwidth});
-},function(){
+    $('.nav>ul>li').hover(function () {
+        $('.nav ul li').removeClass('active');
+        $(this).addClass('active');
+        var currentleft = $('.active').position().left + "px";
+        var currentwidth = $('.active').css('width');
+        $('.lamp').css({"left": currentleft, "width": currentwidth});
+    }, function () {
 
-if($('.nav>ul>li').hasClass('selected')){
-$('.selected').addClass('active');
-var currentleft=$('.selected').position().left+"px";
-var currentwidth=$('.selected').css('width');
-$('.lamp').css({"left":currentleft,"width":currentwidth});
-}
+        if ($('.nav>ul>li').hasClass('selected')) {
+            $('.selected').addClass('active');
+            var currentleft = $('.selected').position().left + "px";
+            var currentwidth = $('.selected').css('width');
+            $('.lamp').css({"left": currentleft, "width": currentwidth});
+        } else {
+            $('.nav>ul>li').first().addClass('active');
+            var currentleft = $('.active').position().left + "px";
+            var currentwidth = $('.active').css('width');
 
-else{
-  $('.nav>ul>li').first().addClass('active');
-  var currentleft=$('.active').position().left+"px";
-var currentwidth=$('.active').css('width');
-
-$('.lamp').css({"left":currentleft,"width":currentwidth});
-}
-});
+            $('.lamp').css({"left": currentleft, "width": currentwidth});
+        }
+    });
 });
