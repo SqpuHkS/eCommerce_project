25384108d19df5fb74317df1df063372a32764cd
SqpuHkS <kovaleksey01@gmail.com>
2020-06-28 19:17:27 +0300

    edit the cart view

--- carts/views.py
+++ carts/views.py
@@ -10,6 +10,9 @@ def cart_home(request):
     qs = Cart.objects.filter(id=cart_id)
     if qs.count() == 1:
         cart_obj = qs.first()
+        if request.user.is_authenticated and cart_obj.user is None:
+            cart_obj.user = request.user
+            cart_obj.save()
         # cart_id exists
     else:
         cart_obj = Cart.objects.new_cart(user=request.user)
