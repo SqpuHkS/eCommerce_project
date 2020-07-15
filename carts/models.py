72a24c85e6cd6913987a7703c64d2f4d85dde915
SqpuHkS <kovaleksey01@gmail.com>
2020-07-15 12:46:11 +0300

    added to cart models a subtotal field and added m2m receiver and pre save receiver

--- carts/models.py
+++ carts/models.py
@@ -1,14 +1,16 @@
 from django.db import models
 from django.conf import settings
 from items.models import Item
+from django.db.models.signals import pre_save, post_save, m2m_changed
 
 # Create your models here.
 User = settings.AUTH_USER_MODEL
 
 class CartManager(models.Manager):
+
     def new_or_get(self, request):
         cart_id = request.session.get('cart_id', None)
-        qs = Cart.objects.filter(id=cart_id)
+        qs = self.get_queryset().filter(id=cart_id)
         if qs.count() == 1:
             new_obj = False
             cart_obj = qs.first()
@@ -28,12 +30,12 @@ class CartManager(models.Manager):
         if user is not None:
             if user.is_authenticated:
                 user_obj = user
-        print(user_obj)
         return self.model.objects.create(user=user_obj)
 
 class Cart(models.Model):
     user = models.ForeignKey(User, null=True, blank=True, on_delete=models.SET_NULL)
     items = models.ManyToManyField(Item, blank=True)
+    subtotal = models.DecimalField(default=0.00, max_digits=100, decimal_places=2)
     total = models.DecimalField(default=0.00, max_digits=100, decimal_places=2)
     updated = models.DateTimeField(auto_now_add=True)
     timestamp = models.DateTimeField(auto_now_add=True)
@@ -42,3 +44,24 @@ class Cart(models.Model):
 
     def __str__(self):
         return str(self.id)
+
+# many to many signal
+def m2m_changed_cart_receiver(sender, instance, action, *args, **kwargs):
+    if action == 'post_add' or action == 'post_remove' or action == 'post_clear':
+        items = instance.items.all()
+        total = 0
+        for i in items:
+            total += i.price
+        # For reason to not repeat and call the save method without any changes
+        if instance.subtotal != total:
+            instance.subtotal = total
+            instance.save()
+
+m2m_changed.connect(m2m_changed_cart_receiver, sender=Cart.items.through)
+
+def cart_pre_save_receiver(sender, instance, *args, **kwargs):
+    instance.total = instance.subtotal 
+
+pre_save.connect(cart_pre_save_receiver, sender=Cart)
+
+
--- carts/views.py
+++ carts/views.py
@@ -3,6 +3,12 @@ from .models import Cart
 
 
 def cart_home(request):
-    cart_obj = Cart.objects.new_or_get(request)
-        # create cart
+    cart_obj, new_obj = Cart.objects.new_or_get(request)
+    items =  cart_obj.items.all()
+    total = 0
+    for i in items:
+        total += i.price
+    # create cart
+    cart_obj.total = total
+    cart_obj.save()
     return render(request, 'carts/home.html')
