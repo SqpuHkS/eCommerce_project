0c4a64f9432ee1beae314cb5898fc857f0c886b7
SqpuHkS <kovaleksey01@gmail.com>
2020-07-18 01:01:26 +0300

    added a functionality to add and remove from cart

--- carts/views.py
+++ carts/views.py
@@ -16,11 +16,16 @@ def cart_home(request):
     return render(request, 'carts/home.html')
 
 def cart_update(request):
-    item_id = 3
-    item_obj = Item.objects.get(id = item_id)
-    cart_obj, new_obj = Cart.objects.new_or_get(request)
-    if item_obj in cart_obj.items.all():
-        cart_obj.items.remove(item_obj)
-    else:
-        cart_obj.items.add(item_obj)
+    item_id = request.POST.get('item_id')
+    if item_id is not None:
+        try:
+            item_obj = Item.objects.get(id = item_id)
+        except Item.DoesNotExist:
+            print('Error')
+            return redirect('cart:home')
+        cart_obj, new_obj = Cart.objects.new_or_get(request)
+        if item_obj in cart_obj.items.all():
+            cart_obj.items.remove(item_obj)
+        else:
+            cart_obj.items.add(item_obj)
     return redirect('cart:home')
