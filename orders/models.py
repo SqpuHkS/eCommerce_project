5f29ec053f906387367765b66e5fb8bae710f433
SqpuHkS <kovaleksey01@gmail.com>
2020-08-08 18:00:45 +0300

    edited the order model

--- orders/models.py
+++ orders/models.py
@@ -1,9 +1,9 @@
 from decimal import Decimal
-
 from django.db import models
-from carts.models import Cart
 from django.db.models.signals import pre_save, post_save
 
+from carts.models import Cart
+from billing.models import BillingProfile
 from eCommerce.utils import unique_order_id_generator
 
 ORDER_STATUS_CHOICES = (
@@ -15,11 +15,13 @@ ORDER_STATUS_CHOICES = (
 
 # Create your models here.
 class Order(models.Model):
+    billing_profile = models.ForeignKey(BillingProfile, blank=True, null=True, on_delete=models.CASCADE)
     order_id = models.CharField(max_length=120, blank=True)
     cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
     status = models.CharField(max_length=120, default='created', choices=ORDER_STATUS_CHOICES)
     shipping_total = models.DecimalField(default=5.99, max_digits=100, decimal_places=2)
     total = models.DecimalField(default=0.00, max_digits=100, decimal_places=2)
+    active = models.BooleanField(default=True)
 
     def __str__(self):
         return self.order_id
