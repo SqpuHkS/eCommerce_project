4f7231797954ec4353d8b8e27d20d829281d2209
SqpuHkS <kovaleksey01@gmail.com>
2020-08-18 15:35:36 +0300

    checkout is workingfine with addresses

--- carts/views.py
+++ carts/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render, redirect
 
 from addresses.forms import AddressForm
+from accounts.forms import GuestForm, LoginForm
+
+from addresses.models import Address
 from billing.models import BillingProfile
 from .models import Cart
 from items.models import Item
 from orders.models import Order
-from accounts.forms import GuestForm, LoginForm
 
 
 def cart_home(request):
@@ -38,14 +40,23 @@ def checkout_home(request):
     login_form = LoginForm()
     guest_form = GuestForm()
     address_form = AddressForm()
-    billing_address_form = AddressForm()
+
+    billing_address_id = request.session.get('billing_address_id', None)
+    shipping_address_id = request.session.get('shipping_address_id', None)
+
 
     billing_profile, billing_profile_created = BillingProfile.objects.new_or_get(request)
 
     if billing_profile is not None:
         order_obj, order_obj_created = Order.objects.new_or_get(billing_profile, cart_obj)
-
-    print(dir(order_obj))
+        if shipping_address_id:
+            order_obj.shipping_address = Address.objects.get(id=shipping_address_id)
+            del request.session['shipping_address_id']
+        if billing_address_id:
+            order_obj.billing_address = Address.objects.get(id=billing_address_id)
+            del request.session['billing_address_id']
+        if billing_address_id or shipping_address_id:
+            order_obj.save()
 
     context = {
         'billing_profile': billing_profile,
@@ -53,7 +64,6 @@ def checkout_home(request):
         'login_form': login_form,
         'guest_form': guest_form,
         'address_form': address_form,
-        'billing_address_form': billing_address_form,
     }
 
 
