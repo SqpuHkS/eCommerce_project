2877e4d734f0a660059befa2836949192caca1a8
SqpuHkS <kovaleksey01@gmail.com>
2020-09-03 16:46:49 +0300

    added a stripe lib and added a pre_save to billing

--- billing/models.py
+++ billing/models.py
@@ -1,8 +1,12 @@
 from django.db import models
 from django.conf import settings
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, pre_save
 
 from accounts.models import GuestEmail
+from conf import token
+
+import stripe
+stripe.api_key = token
 
 User = settings.AUTH_USER_MODEL
 
@@ -34,16 +38,23 @@ class BillingProfile(models.Model):
     active = models.BooleanField(default=True)
     update = models.DateTimeField(auto_now=True)
     timestamp = models.DateTimeField(auto_now_add=True)
+    customer_id = models.CharField(max_length=120, blank=True, null=True)
 
     def __str__(self):
         return self.email
 
     objects = BillingProfileManager()
 
-# def billing_profile_created_receiver(sender, instance, created, *args, **kwargs):
-#     if created:
-#         instance.customer_id = newID
-#         instance.save()
+def billing_profile_created_receiver(sender, instance, *args, **kwargs):
+    if not instance.customer_id:
+        customer = stripe.Customer.create(
+            email=instance.email
+        )
+        print(customer)
+        instance.customer_id = customer.id
+
+
+pre_save.connect(billing_profile_created_receiver, sender=BillingProfile)
 
 
 def user_created_receiver(sender, instance, created, *args, **kwargs):
@@ -51,3 +62,5 @@ def user_created_receiver(sender, instance, created, *args, **kwargs):
         BillingProfile.objects.get_or_create(user=instance, email=instance.email)
 
 post_save.connect(user_created_receiver, sender=User)
+
+
