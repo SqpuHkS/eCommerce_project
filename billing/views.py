403d80ee7f93565bcf04b7e30c18dffdbe820175
SqpuHkS <kovaleksey01@gmail.com>
2020-09-03 18:56:56 +0300

    added form for payment

--- billing/views.py
+++ billing/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+STRIPE_PUB_KEY = 'pk_test_51HNHrgBlsosESXXHVC5Yy5mNFd04M0DK9PMfZROJPgLTkF1oF2Na9vDfXsDoEOAXXD9xUQuDeZAs3HfoE2ZQkCDI007J2P3vXi'
+
+def payment_method_view(request):
+    if request.method == 'POST':
+        print(request.POST)
+    return render(request, 'billing/payment-method.html', context={})
--- eCommerce/urls.py
+++ eCommerce/urls.py
@@ -1,35 +1,23 @@
-"""eCommerce URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import  include
 from django.conf.urls import url
 from django.conf import settings
 from django.conf.urls.static import static
+
 from items.views import *
 from carts.views import *
 from eCommerce.views import *
+from billing.views import *
 
 urlpatterns = [
     url('admin/', admin.site.urls),
     url('account/', include('accounts.urls', namespace='account')),
     url('cart/', include('carts.urls', namespace='cart')),
-    url('checkout/address/', include('addresses.urls', namespace='address')),
     url('items/', include('items.urls', namespace='items')),
     url('search/', include('search.urls', namespace='search')),
     url('api/cart/', cart_detail_api_view, name='cart-api'),
+    url('billing/payment-method', payment_method_view, name='payment-method'),
+    url('checkout/address/', include('addresses.urls', namespace='address')),
     url('^$', main_page, name='main-page-url'),
 ]
 
